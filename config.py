e444bfd7b7d348ed36a713941256b88d1c38daf7
APURAV KASHYAP <24f2000063@ds.study.iitm.ac.in>
2026-02-14 23:26:43 +0530

    Milestone-0 PPA-MAD-1

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-please-change'
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'site.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    
